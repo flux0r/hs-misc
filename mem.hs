0972db56a99252a334bffa914e840fb824223964
flux0r <flux0r@brettmingalls.com>
2013-10-30 08:39:45 -0700

    onExit.

--- mem.hs
+++ mem.hs
@@ -1,45 +1,49 @@
 {-# LANGUAGE KindSignatures #-}
 
 import Control.Applicative (Applicative, (<$>), (<*>), pure)
-import Control.Concurrent.STM (STM, TQueue, TVar, newTVar, writeTQueue)
+import Control.Concurrent.STM (STM, TQueue, TVar, atomically, newTVar,
+                               modifyTVar, writeTQueue)
 import Control.Monad.Reader (ReaderT (ReaderT))
 import Control.Monad.Trans (MonadIO, MonadTrans, lift, liftIO)
 
 data Finalizer = MkFinalizer
     { finalize      :: !(IO ())
-    , refCount      :: !(TVar Int)
+    , ref_cnt       :: !(TVar Int)
     }
 
 newtype RegionT s m a = MkRegionT
-    { unRegionT :: ReaderT (TQueue Finalizer) m a }
+    { un_region :: ReaderT (TQueue Finalizer) m a }
 
 newtype FinalizerHandle (r :: * -> *) = FinalizerHandle Finalizer
 
-mapR :: Functor f => (a -> b) -> RegionT s f a -> RegionT s f b
-mapR f (MkRegionT m) = MkRegionT (f <$> m)
+map_region :: Functor f => (a -> b) -> RegionT s f a -> RegionT s f b
+map_region f (MkRegionT m) = MkRegionT (f <$> m)
 
-unitR :: Monad f => a -> RegionT s f a
-unitR x = MkRegionT (return x)
+unit_region :: Monad f => a -> RegionT s f a
+unit_region x = MkRegionT (return x)
 
-apR :: (Applicative f, Monad f)
+ap_region :: (Applicative f, Monad f)
     => RegionT s f (a -> b)
     -> RegionT s f a
     -> RegionT s f b
-(MkRegionT ff) `apR` (MkRegionT fx) = MkRegionT $ ff <*> fx
+(MkRegionT ff) `ap_region` (MkRegionT fx) = MkRegionT $ ff <*> fx
 
-bindR :: Monad f => RegionT s f a -> (a -> RegionT s f b) -> RegionT s f b
-(MkRegionT m) `bindR` f = MkRegionT $ m >>= unRegionT . f
+bind_region :: Monad f
+            => RegionT s f a
+            -> (a -> RegionT s f b)
+            -> RegionT s f b
+(MkRegionT m) `bind_region` f = MkRegionT $ m >>= un_region . f
 
 instance Functor f => Functor (RegionT s f) where
-    fmap = mapR
+    fmap = map_region
 
 instance (Applicative f, Monad f) => Applicative (RegionT s f) where
-    pure    = unitR
-    (<*>)   = apR
+    pure    = unit_region
+    (<*>)   = ap_region
 
 instance (Applicative f, Monad f) => Monad (RegionT s f) where
-    return  = unitR
-    (>>=)   = bindR
+    return  = unit_region
+    (>>=)   = bind_region
 
 instance (Applicative f, MonadIO f) => MonadIO (RegionT s f) where
     liftIO = MkRegionT . liftIO
@@ -47,22 +51,30 @@ instance (Applicative f, MonadIO f) => MonadIO (RegionT s f) where
 instance MonadTrans (RegionT s) where
     lift = MkRegionT . lift
 
---onExit cleanup = MkRegionT $ ReaderT $
+class Duplicate h where
+    duplicate :: (MonadIO f, Applicative f)
+              => h (RegionT c (RegionT p f))
+              -> RegionT c (RegionT p f) (h (RegionT p f))
 
---f :: IO () -> STM Finalizer
-f cleanup = newTVar 1 >>= return . MkFinalizer cleanup
+instance Duplicate FinalizerHandle where
+    duplicate = lift . copy
 
---g :: TQueue Finalizer -> Finalizer -> STM (FinalizerHandle r)
---g q h = h >>= \h' -> writeTQueue q h' >> return $ FinalizerHandle h
+onExit :: MonadIO f => IO () -> RegionT s f (FinalizerHandle (RegionT s f))
+onExit cleanup = MkRegionT $ ReaderT $ \q ->
+    liftIO . atomically $ newTVar 1 >>= \cnt ->
+    let h = MkFinalizer (liftIO cleanup) cnt
+    in  writeTQueue q h >>
+        return (FinalizerHandle h)
 
---h cleanup q = g q (f cleanup)
+inc_ref_cnt :: TVar Int -> STM ()
+inc_ref_cnt x = modifyTVar x (+ 1)
 
---onExit cleanup = MkRegionT $ ReaderT $ \q -> g q (f cleanup)
-
-onExit cleanup = MkRegionT $ ReaderT $ \q -> return $
-    let cleanup' = liftIO cleanup
-    in  do
-            cnt <- newTVar 1
-            let h = MkFinalizer cleanup' cnt
-            writeTQueue q h
-            return $ FinalizerHandle h
+copy :: MonadIO f
+     => FinalizerHandle a
+     -> RegionT s f (FinalizerHandle (RegionT s f))
+copy (FinalizerHandle h) =
+    MkRegionT $ ReaderT $ \q ->
+    (liftIO . atomically)
+    ((inc_ref_cnt . ref_cnt) h >>
+    writeTQueue q h >>
+    return (FinalizerHandle h))
