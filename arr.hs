ac90766858e4027e6fefc59cbf7030cb1b2bb90f
flux0r <flux0r@brettmingalls.com>
2013-12-13 18:52:52 -0800

    Work on array interface.

--- arr.hs
+++ arr.hs
@@ -1,7 +1,18 @@
-{-# LANGUAGE NoImplicitPrelude, TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts, NoImplicitPrelude, TypeFamilies #-}
 
-class Array a where
+import Prelude (Integral, Real)
+
+class (Real (Measure a)) => Measured a where
+    type Measure a :: *
+    measure :: a -> Measure a
+
+class Semigroup a where
+    (<>) :: a -> a -> a
+
+class (Measured a, Integral (Idx a)) => Array a where
     type Idx a :: *
     type Elt a :: *
     ref :: a -> Idx a -> Elt a
     set :: a -> Idx a -> Elt a -> a
+    length :: a -> Measure a
+    fill :: a -> Elt a -> a
